2d0a0ed86b11ad802add804fbd845ae41a9f5558
karthik raja <37881042+mkraja826@users.noreply.github.com>
2026-07-05 04:38:25 +0530

    Set client company names

--- src/clients.js
+++ src/clients.js
@@ -1,1 +1,5 @@
-export const clients = [];
+export const clients = [
+  { name: 'Clinicaprollc', website: 'clinicaprollc.com', status: 'US Staffing Company', icon: '💼', description: 'US staffing company.' },
+  { name: 'Sicada Digital', website: 'sicadadigital.com', status: 'Software Company', icon: '💻', description: 'Software company.' },
+  { name: 'B.G Reddy Dental Clinic', website: 'bgreddydentalclinic.com', status: 'Dental Clinic', icon: '🦷', description: 'Dental clinic.' },
+];
